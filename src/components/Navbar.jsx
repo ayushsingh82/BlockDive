04b3251de4d76ccad5abb07265507432851e4eac
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-10-15 12:48:25 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useState} from 'react'
 // import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 
 const gradientStyle = {
   background: 'linear-gradient(to right, #45E1E5, #0052FF, #B82EA4, #FF9533, #7FD057, #45E1E5)',
@@ -9,6 +10,13 @@ const gradientStyle = {
 };
 
 function Navbar() {
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
     return (
       <nav className=''>
         <div className='flex flex-row mx-auto px-[40px] py-[25px] justify-between items-center mt-[0px] bg-black'>
@@ -43,9 +51,58 @@ function Navbar() {
     <h1 className='font-medium text-lg text-white'>
     <a href='/ethtoken'>EthToken</a>
   </h1>
-          </div>
-          <div className='text-white'>
-          Connect
+
+    {/* Dropdown button */}
+    <div className='relative justify-end'>
+    <button
+      onClick={toggleDropdown}
+      className='bg-green-500 text-white px-4 py-2 rounded-md'>
+      Get Started
+    </button>
+
+    {/* Dropdown Menu */}
+    {dropdownOpen && (
+      <div className='absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg'>
+        <a
+          href='/aptosaccounts'
+          className='block px-4 py-2 text-black hover:bg-gray-200'>
+          Aptos Account
+        </a>
+        <a
+          href='/aptostxns'
+          className='block px-4 py-2 text-black hover:bg-gray-200'>
+          Aptos Transactions
+        </a>
+        <a
+          href='/ethnft'
+          className='block px-4 py-2 text-black hover:bg-gray-200'>
+          Ethereum NFT
+        </a>
+        <a
+        href='/ethtoken'
+        className='block px-4 py-2 text-black hover:bg-gray-200'>
+        Ethereum Token
+      </a>
+        <a
+          href='/base'
+          className='block px-4 py-2 text-black hover:bg-gray-200'>
+          Base
+        </a>
+        <a
+          href='/arbitrum'
+          className='block px-4 py-2 text-black hover:bg-gray-200'>
+          Arbitrum
+        </a>
+        <a
+        href='/explorer'
+        className='block px-4 py-2 text-black hover:bg-gray-200'>
+        Explorer
+      </a>
+      </div>
+    )}
+  </div>
+
+
           </div>
         </div>
         <div style={gradientStyle} />
